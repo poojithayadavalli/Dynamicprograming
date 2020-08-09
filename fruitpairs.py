4e7fc08cde793eaee56eee7a505e604c221325ef
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-09 14:33:39 +0530

    Update fruitpairs.py

--- fruitpairs.py
+++ fruitpairs.py
@@ -12,6 +12,7 @@ Output:
 
 print the total possible ways
 
+
 Example1:
 
 Input:
@@ -20,6 +21,12 @@ Input:
 output:
 4
 
+Explanation:
+{1}, {2}, {3} : all single
+{1}, {2, 3} : 2 and 3 paired but 1 is single.
+{1, 2}, {3} : 1 and 2 are paired but 3 is single.
+{1, 3}, {2} : 1 and 3 are paired but 2 is single.
+Note that {1, 2} and {2, 1} are considered same.
 Example 2:
 
 Input:
@@ -41,5 +48,6 @@ def countFruitsPairings(n):
             dp[i] = dp[i - 1] + (i - 1) * dp[i - 2] 
   
     return dp[n] 
- n=int(input())
- print(countFruitsPairings(n))
+n=int(input())
+print(countFruitsPairings(n))
+
