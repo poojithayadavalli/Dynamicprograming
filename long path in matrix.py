04dfc1741518b502b1cbb10b7e06e5e4e4b9215d
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-08-09 15:18:59 +0530

    Update long path in matrix.py

--- long path in matrix.py
+++ long path in matrix.py
@@ -1,8 +1,10 @@
 """
 
-Given a n*n matrix where all numbers are distinct, find the maximum length path (starting from any cell) such that all cells along the path are in increasing order with a difference of 1.
+Given a n*n matrix where all numbers are distinct, find the maximum length path (starting from any cell) such that all cells
 
-We can move in 4 directions from a given cell (i, j), i.e., we can move to (i+1, j) or (i, j+1) or (i-1, j) or (i, j-1) with the condition that the adjacent cells have a difference of 1.
+along the path are in increasing order with a difference of 1. We can move in 4 directions from a given cell (i, j), 
+
+i.e., we can move to (i+1, j) or (i, j+1) or (i-1, j) or (i, j-1) with the condition that the adjacent cells have a difference of 1.
 
 Input:
 Firstline indicates matrix size N
